ef6013045845223d82ed68cb947bd3f97a9419a0
Jack Winter <jack.winter@kto.com>
2026-02-10 15:58:36 +0700

    Refactoring code to use tailwind

---
+++ src/lib/nav.ts
@@ -0,0 +1,6 @@
+export const nav = [
+  { href: "/", label: "Home" },
+  { href: "/profile", label: "Profile" },
+  { href: "/work", label: "Work" },
+  { href: "/contact", label: "Contact" },
+] as const;
